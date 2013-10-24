1920525dbcad87a0ee19c4fd6893358269fbdc2f
sebastian <sebastianthomas1@live.com>
2013-10-24 18:20:56 +0530

    sadasdas

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,11 +2,13 @@ class Student < ActiveRecord::Base
   belongs_to :user
   mount_uploader :picture, PictureUploader
   mount_uploader :resume, ResumeUploader
+
+  validates_presence_of :name, :course, :dept, :batch, :gender, :ug_cgpa, :dob, :fname, :mname, :fjob, :mjob, :paddr, :mobile, :email
   #before_save :adv_modify
 
     COURSE = ['B.Tech','M.Tech','B.Arch','MCA']
 	DEPT = ['Computer Science','Chemical','Civil','Electrical' ,'Electronics','Mechanical','Production','Architecture','MCA']
-	BATCH = ['2010-14','2011-15','2012-16','2013-17']
+	BATCH = ['2010-14','2011-15','2012-16','2013-17', '2012-14','2013-15','2014-16']
 
 
 	def self.to_csv(options = {})
